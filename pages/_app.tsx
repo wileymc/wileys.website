267b026ea5a42fbc7df6ce8d2ef276b0802d7b2e
Wiley Conte <wileymckayconte@gmail.com>
2023-02-07 12:49:05 -0700

    First commit, rewrite

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto, Merriweather } from "@next/font/google";
+import clsx from "clsx";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+const merriweather = Merriweather({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-merriweather",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={clsx(roboto.variable, merriweather.variable)}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
